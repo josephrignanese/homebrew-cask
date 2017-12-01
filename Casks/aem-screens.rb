50281012acb9d9eadebec4b97eb058195cb609a3
Joseph Rignanese <rignanes@adobe.com>
2017-12-01 13:27:41 +1100

    Add AEM Screens Player for OSX v3.2.68

---
+++ Casks/aem-screens.rb
@@ -0,0 +1,10 @@
+cask 'aem-screens' do
+  version '3.2.68'
+  sha256 '2e77eb2e84e6c804ef3d6388d8f43d1d5e797faaafc26feca2d5b4336e542bd6'
+
+  url 'https://download.macromedia.com/screens/download/aem-screens-player-osx-3.2.68.dmg'
+  name 'AEM Screens Player for OSX'
+  homepage 'https://download.macromedia.com/screens/'
+
+  app 'AEM Screens.app'
+end
